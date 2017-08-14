540e5d4f299fe99adb9f0ae0c8cee7e9d041f6b4
Riski Zulfiansyah <riski.zulfiansyah@nalar.id>
2017-08-14 11:17:11 +0700

    Edit BankTableSeed

--- database/seeds/BankTableSeeder.php
+++ database/seeds/BankTableSeeder.php
@@ -20,11 +20,7 @@ class BankTableSeeder extends Seeder
         $bank = new Bank();
         $bank->name = "Mandiri";
         $bank->account_name = "Arina Sani";
-<<<<<<< HEAD
         $bank->account_number = "";
-=======
-        $bank->account_number = "130001502303";
->>>>>>> b057535a661bd2feba77ee9a3e242677c836e7bb
         $bank->save();
         $bank = new Bank();
         $bank->name = "BNI";
