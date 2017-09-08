aae5c5d9711188005576ed57c3404df9e5be9358
Rizky Hamdhany <rizky@ebizu.com>
2017-09-08 18:14:10 +0700

    update doku

--- resources/views/testpay.blade.php
+++ resources/views/testpay.blade.php
@@ -1,12 +1,23 @@
 <FORM NAME="order" METHOD="Post" ACTION="https://apps.myshortcart.com/payment/request-payment/" >
-    <input type="text" name="BASKET" value="SMTICKET,70000,1,70000">
-    <input type="text" name="STOREID" value="10589598">
-    <input type="text" name="TRANSIDMERCHANT" value="000001">
-    <input type="text" name="AMOUNT" value="70000">
-    <input type="text" name="URL" value="http://nalar-ventex.com/">
-    <input type="text" name=”WORDS” value="{{sha1("75000"."J7h9X7i7F4D8"."000001")}}">
-    <input type="text" name="CNAME" value="Ismail Danuarta">
-    <input type="text" name="CEMAIL" value="ismail@gmail.com">
-    <input type="text" name="CMPHONE" value="081298098090">
+    <input type=text" name="BASKET" value="Gold,70000.00,1,70000.00;Administration fee,5000.00,1,5000.00">
+    <input type=text" name="STOREID" value="10589598">
+    <input type=text" name="TRANSIDMERCHANT" value="000001">
+    <input type=text" name="AMOUNT" value="75000.00">
+    <input type=text" name="URL" value="http://www.yourwebsite.com/ ">
+    <input type=text" name="WORDS" value="e8270e972e4049394dafdcacaa938a48a1949168">
+    <input type=text" name="CNAME" value="Ismail Danuarta">
+    <input type=text" name="CEMAIL" value="ismail@gmail.com">
+    <input type=text" name="CWPHONE" value="0210000011">
+    <input type=text" name="CHPHONE" value="0210980901">
+    <input type=text" name="CMPHONE" value="081298098090">
+    <input type=text" name="CCAPHONE" value="02109808009">
+    <input type=text" name="CADDRESS" value="Jl. Jendral Sudirman Plaza Asia Office Park Unit 3">
+    <input type=text" name="CZIPCODE" value="12345">
+    <input type=text" name="SADDRESS" value="Pengadegan Barat V no 17F">
+    <input type=text" name="SZIPCODE" value="12217">
+    <input type=text" name="SCITY" value="JAKARTA">
+    <input type=text" name="SSTATE" value="DKI">
+    <input type=text" name="SCOUNTRY" value="784">
+    <input type=text" name="BIRTHDATE" value="1988-06-16">
     <button type="submit">Pay</button>
 </FORM>
