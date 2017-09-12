ed13613d23f5bc187a4bad43089cdbae43261e62
Riski <soliduszeld@gmail.com>
2017-09-12 21:01:33 +0700

    Change number format

--- resources/views/dashboard/admin/event/event_ticketCategory.blade.php
+++ resources/views/dashboard/admin/event/event_ticketCategory.blade.php
@@ -59,7 +59,7 @@
                                       <div class="ribbon ribbon-right ribbon-color-default uppercase" style="font-size:0.7em;">{{$period->start_date}} / {{$period->end_date}}</div>
                                         @forelse($period->ticketClass as $class)
                                           <div class="ribbon-content">
-                                            {{$class->name}}:IDR {{number_format($class->price,2)}}
+                                            {{$class->name}} : IDR {{number_format($class->price,0, "",".")}}
                                             <a href="{{route('dashboard.event.ticketClass.edit',[$id,$class->id])}}" class="btn btn-circle btn-icon-only yellow circle-style"><i class="icon-wrench" ></i></a><a href="{{route('dashboard.event.ticketClass.delete',[$id,$class->id])}}" data-period-del="{{$class->id}}" class="btn btn-circle btn-icon-only red circle-style"><i class="icon-trash" ></i></a>
                                           </div>
 
@@ -69,7 +69,7 @@
                                   </div>
                               </div>
                               @empty
-                                <?php print "No Category" ?>
+                                <?php print "No Category Data" ?>
                               @endforelse
                             </div>
                         </div>
