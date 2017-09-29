83ec5e617fc47ccf8e812179e14527d874b9d626
Rizky Hamdhany <rizky@ebizu.com>
2017-09-29 15:36:38 +0700

    ticket api

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -37,4 +37,25 @@ class TicketController extends Controller
         $tickets = $this->ticketRepo->findWhere(["event_id" => $event_id], ["ticket_code", "name", "phonenumber", "email"]);
         return response()->json(['status' => 'success', 'message' => 'login success','data' => compact('tickets')]);
     }
+
+    public function ticketChecking(Request $request){
+        $user = JWTAuth::parseToken()->authenticate();
+        if (!$user) {
+            return response()->json(['status' => 'error', 'message' => 'invalid_credentials'], 404);
+        }
+        $event_id = $user->event_id;
+        $ticket = $this->ticketRepo->findWhere(["event_id" => $event_id, "ticket_code" => $request->input('ticket_code')])->first();
+        if (isset($ticket)){
+            if ($ticket->ticket_checking == ""){
+                $status = "Valid";
+                $ticket->ticket_checking = "Used";
+                $ticket->save();
+            } else{
+                $status = "Used";
+            }
+        } else {
+            $status = "Not Found";
+        }
+        return response()->json(['status' => 'success', 'message' => 'check success','data' => compact('status')]);
+    }
 }
--- database/seeds/old/RoleTableSeeder.php
+++ database/seeds/old/RoleTableSeeder.php
@@ -16,14 +16,12 @@ class RoleTableSeeder extends Seeder
         $role_employee->name = 'superadmin';
         $role_employee->description = 'Nalar TMS Admin';
         $role_employee->save();
-        $role_manager = new Role();
 
+        $role_manager = new Role();
         $role_manager->name = 'ftb-operator';
         $role_manager->description = 'Festival Budaya Operator';
-
         $role_manager->name = 'eo';
         $role_manager->description = 'Smilemotion Operator';
-
         $role_manager->save();
     }
 }
